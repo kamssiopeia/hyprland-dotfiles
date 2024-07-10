28a9b48a660e2fff5a7e9aa90ea7b71f8ee9f89c
Kamil Golański <kamilgol1@gmail.com>
2024-07-10 16:56:57 +0200

    added logs and waybar reloading

--- .config/hypr/scripts/change_wallpaper.sh
+++ .config/hypr/scripts/change_wallpaper.sh
@@ -3,6 +3,15 @@
 WALLPAPERS_DIR=~/Wallpapers
 
 random_wallpaper=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
+echo "Randomized wallpaper [${random_wallpaper}]"
+
 hyprctl hyprpaper preload "$random_wallpaper"
 hyprctl hyprpaper wallpaper ",$random_wallpaper"
-wal -i "$random_wallpaper"
+echo "Changed wallpaper"
+
+wal -n -s -i "$random_wallpaper"
+echo "Generated color palette"
+
+source waybar_reload.sh > /dev/null 2>&1
+echo "Reloaded waybar"
+
