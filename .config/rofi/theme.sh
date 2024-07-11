68a42d7772c4af332e40ec6e5272a2b8f2ae7046
Kamil Golański <kamilgol1@gmail.com>
2024-07-11 13:24:39 +0200

    improved rofi theme picker script to look for files in both dark and light directories

--- .config/rofi/theme.sh
+++ .config/rofi/theme.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-rofi_theme="$HOME/.config/rofi/launcher/style"
+rofi_theme=$HOME/.config/rofi/launcher/style
+themes_dir=$HOME/.config/wal/colorschemes
 
-themes_dir=$HOME/.config/wal/colorschemes/dark
-selected_theme="$(ls $themes_dir | sort | rofi -dmenu -i -p " " -theme ${rofi_theme})"
-selected_theme_path=$themes_dir/$selected_theme
+selected_theme="$(find $themes_dir -type f -name '*.json' -printf "%P\n" | sort | rofi -dmenu -i -p " " -theme ${rofi_theme})"
 
-if [[ $selected_theme_path == "" ]]; then
+if [[ $selected_theme == "" ]]; then
     exit 1
 fi
 
-if [[ -f $selected_theme_path ]]; then
-    echo "$selected_theme_path selected"
-elif [[ -d $selected_theme_path ]]; then
-    echo "$selected_theme_path is a directory"
-    exit 1
-else
-    echo "$selected_theme_path is not valid"
-    exit 1
-fi
-
-sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -t $selected_theme_path
+sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -t $themes_dir/$selected_theme
