9391d1e2536fdfa583fea9822747e61f585b0b50
Kamil Golański <kamilgol1@gmail.com>
2024-07-11 18:38:26 +0200

    added thumbnails to wallpaper picker

--- .config/rofi/wallpaper.sh
+++ .config/rofi/wallpaper.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-rofi_theme=$HOME/.config/rofi/launcher/style
-wallpapers_dir=$HOME/Wallpapers
+ROFI_THEME=$HOME/.config/rofi/launcher/style
+CACHE_DIR=$HOME/.cache/rofi_wallpaper_picker
+WALLPAPERS_DIR=$HOME/Wallpapers
 
-selected_wallpaper="$(find $wallpapers_dir -type f -printf "%P\n" | sort | rofi -dmenu -i -p " " -theme ${rofi_theme})"
+if [ ! -d "${CACHE_DIR}" ] ; then
+    mkdir -p "${CACHE_DIR}"
+fi
+
+for wallpaper in "$WALLPAPERS_DIR"/*.{jpg,jpeg,png,webp}; do
+    if [ -f "$wallpaper" ]; then
+        wallpaper_name=$(basename "$wallpaper")
+        if [ ! -f "${CACHE_DIR}/${wallpaper_name}" ] ; then
+            magick "$wallpaper" -thumbnail '320x180>' "${CACHE_DIR}/${wallpaper_name}"
+        fi
+    fi
+done
+
+selected_wallpaper=$(find "${WALLPAPERS_DIR}" -type f -printf "%P\n" | sort | while read -r A ; do echo -en "$A\x00icon\x1f""${CACHE_DIR}"/"$A\n" ; done | rofi -dmenu -i -p " " -theme ${ROFI_THEME})
 
 if [[ $selected_wallpaper == "" ]]; then
     exit 1
 fi
 
-selected_wallpaper_path=$wallpapers_dir/$selected_wallpaper
+selected_wallpaper_path=$WALLPAPERS_DIR/$selected_wallpaper
 
 sh $HOME/.config/hypr/scripts/wallpaper_changer.sh $selected_wallpaper_path
 sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -g $selected_wallpaper_path
