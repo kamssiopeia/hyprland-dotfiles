be08d55aa4f60ab0c977508935a7717788fffcd2
Kamil Golański <kamilgol1@gmail.com>
2024-07-16 23:29:45 +0200

    changed constants to capital letters

--- .config/rofi/theme.sh
+++ .config/rofi/theme.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-rofi_theme=$HOME/.config/rofi/theme/style
-themes_dir=$HOME/.config/wal/colorschemes
+ROFI_THEME=$HOME/.config/rofi/theme/style
+THEMES_DIR=$HOME/.config/wal/colorschemes
+CURR_WALL_ENTRY=" Use current wallpaper theme"
 
-curr_wall_entry=" Use current wallpaper theme"
-themes=$curr_wall_entry$'\n'
-themes+=$(find $themes_dir -type f,l -name '*.json' -printf "%P\n" | sort)
+themes=$CURR_WALL_ENTRY$'\n'
+themes+=$(find $THEMES_DIR -type f,l -name '*.json' -printf "%P\n" | sort)
 
-selected_theme=$(echo "$themes" | rofi -dmenu -i -p " " -theme ${rofi_theme})
+selected_theme=$(echo "$themes" | rofi -dmenu -i -p " " -theme ${ROFI_THEME})
 
 if [[ $selected_theme == "" ]]; then
     exit 1
-elif [[ $selected_theme == $curr_wall_entry ]]; then
+elif [[ $selected_theme == $CURR_WALL_ENTRY ]]; then
     wallpaper_path=$(cat $HOME/.cache/wal/wal)
     sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -g $wallpaper_path
 else
-    sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -t $themes_dir/$selected_theme
+    sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -t $THEMES_DIR/$selected_theme
 fi
