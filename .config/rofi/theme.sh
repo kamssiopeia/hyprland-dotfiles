3d0871b7f966a2d40d1a56edd226a6533a95b0ef
Kamil Golański <kamilgol1@gmail.com>
2024-07-16 23:28:30 +0200

    improved condition to use variable and added bolt icon to better recognize entry

--- .config/rofi/theme.sh
+++ .config/rofi/theme.sh
@@ -3,17 +3,17 @@
 rofi_theme=$HOME/.config/rofi/theme/style
 themes_dir=$HOME/.config/wal/colorschemes
 
-themes=$'Use current wallpaper theme\n'
+curr_wall_entry=" Use current wallpaper theme"
+themes=$curr_wall_entry$'\n'
 themes+=$(find $themes_dir -type f,l -name '*.json' -printf "%P\n" | sort)
 
 selected_theme=$(echo "$themes" | rofi -dmenu -i -p " " -theme ${rofi_theme})
 
 if [[ $selected_theme == "" ]]; then
     exit 1
-elif [[ $selected_theme == "Use current wallpaper theme" ]]; then
+elif [[ $selected_theme == $curr_wall_entry ]]; then
     wallpaper_path=$(cat $HOME/.cache/wal/wal)
     sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -g $wallpaper_path
-    exit 1
+else
+    sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -t $themes_dir/$selected_theme
 fi
-
-sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -t $themes_dir/$selected_theme
