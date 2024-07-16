482085ca2bcee1cc831ec4767fad17b7fbe19fcf
Kamil Golański <kamilgol1@gmail.com>
2024-07-16 23:20:20 +0200

    added generating theme from current wallpaper

--- .config/rofi/theme.sh
+++ .config/rofi/theme.sh
@@ -3,9 +3,16 @@
 rofi_theme=$HOME/.config/rofi/theme/style
 themes_dir=$HOME/.config/wal/colorschemes
 
-selected_theme="$(find $themes_dir -type f,l -name '*.json' -printf "%P\n" | sort | rofi -dmenu -i -p " " -theme ${rofi_theme})"
+themes=$'Use current wallpaper theme\n'
+themes+=$(find $themes_dir -type f,l -name '*.json' -printf "%P\n" | sort)
+
+selected_theme=$(echo "$themes" | rofi -dmenu -i -p " " -theme ${rofi_theme})
 
 if [[ $selected_theme == "" ]]; then
+    exit 1
+elif [[ $selected_theme == "Use current wallpaper theme" ]]; then
+    wallpaper_path=$(cat $HOME/.cache/wal/wal)
+    sh $HOME/.config/hypr/scripts/pywal_wrapper.sh -g $wallpaper_path
     exit 1
 fi
 
