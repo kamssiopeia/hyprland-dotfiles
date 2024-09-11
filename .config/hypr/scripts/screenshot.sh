0a16ddcfc33d89f758748c8f6ae12c9ebb2fa50f
Kamil Golański <kamilgol1@gmail.com>
2024-09-11 11:01:04 +0200

    added slurp background color and fixed screenshots not being copied to clipboard

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grim -g "$(slurp -d -w 0)" -t jpeg -q 100 - | wl-copy -t image/jpeg && wl-paste > ~/Pictures/$(date +%Y%m%d%H%m%S)_grim.jpg
+grim -g "$(slurp -d -w 0 -b "##CCCCFF4D")" -t jpeg -q 100 - | wl-copy -t image/png && wl-paste > ~/Pictures/$(date +%Y%m%d%H%m%S)_grim.jpg
--- .config/hypr/scripts/screenshot_clipboard.sh
+++ .config/hypr/scripts/screenshot_clipboard.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grim -g "$(slurp -d -w 0)" -t jpeg -q 100 - | wl-copy
+grim -g "$(slurp -d -w 0 -b "#CCCCFF4D")" -t jpeg -q 100 - | wl-copy -t image/png
