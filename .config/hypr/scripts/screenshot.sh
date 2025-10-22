4807fb14d7aca87a9d320e98e15fd8e50a950827
Kamil Golański <kamilgol1@gmail.com>
2025-10-22 16:30:15 +0200

    added satty

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-grim -g "$(slurp -d -w 0 -b "#CCCCFF4D")" -t jpeg -q 100 ~/Pictures/$(date +%Y%m%d%H%m%S)_grim.jpg
+SCREENSHOT_DIR=~/Pictures/
+REGION=$(slurp -d -w 0 -b "#CCCCFF4D")
+
+grim -g "$REGION" - |
+  satty --filename - \
+    --output-filename "$SCREENSHOT_DIR/screenshot-$(date +%Y%m%d%H%m%S).png" \
+    --early-exit \
+    --actions-on-enter save-to-clipboard \
+    --save-after-copy \
+    --copy-command 'wl-copy'
