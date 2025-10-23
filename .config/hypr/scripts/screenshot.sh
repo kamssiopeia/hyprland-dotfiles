a3f21249790a9df1cd1f85a1c221c44584fcb6ee
Kamil Golański <kamilgol1@gmail.com>
2025-10-23 11:10:07 +0200

    removed shreenshot_clipboard.sh  and moved its functionality to screenshot.sh script

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-SCREENSHOT_DIR=~/Pictures/
+save_to_file=${1:-false}
+
+SCREENSHOT_DIR=~/Pictures
 REGION=$(slurp -d -w 0 -b "#CCCCFF4D")
 
+satty_args=(
+  --filename -
+  --early-exit
+  --actions-on-enter save-to-clipboard
+  --copy-command 'wl-copy'
+)
+
+if [[ $save_to_file == true ]]; then
+  satty_args+=(
+    --output-filename "$SCREENSHOT_DIR/screenshot-$(date +%Y%m%d%H%m%S).png"
+    --save-after-copy
+  )
+fi
+
 grim -g "$REGION" - |
-  satty --filename - \
-    --output-filename "$SCREENSHOT_DIR/screenshot-$(date +%Y%m%d%H%m%S).png" \
-    --early-exit \
-    --actions-on-enter save-to-clipboard \
-    --save-after-copy \
-    --copy-command 'wl-copy'
+  satty "${satty_args[@]}"
